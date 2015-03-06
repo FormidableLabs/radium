60f44997f52dc068311cb5da1a0d15a5e409c33f
Alex Lande <alexlande@gmail.com>
2015-03-06 11:45:12 -0800

    Optimize minified webpack config a little more

--- webpack.config.minified.js
+++ webpack.config.minified.js
@@ -1,14 +1,21 @@
 var webpack = require('webpack');
 var _ = require('lodash');
 
-module.exports = _.merge(require('./webpack.config.js'), {
+module.exports = _.merge({}, require('./webpack.config.js'), {
   output: {
     filename: 'radium.min.js'
   },
   plugins: [
+    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        // Signal production mode for React JS and other libs.
+        NODE_ENV: JSON.stringify("production")
       }
     })
   ]
