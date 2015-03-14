a283ea49c420e55c930398702da8879afd5f6807
Alex Lande <alexlande@gmail.com>
2015-03-13 19:36:33 -0700

    Add ESLint

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+gulp.task('eslint', function () {
+  gulp.src('./modules/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.formatEach('stylish', process.stderr))
+    .pipe(eslint.failOnError());
+});
