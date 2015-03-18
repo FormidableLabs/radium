c152f136140faffbaa7d49d7e2db646d4afa7f10
Alex Lande <alexlande@gmail.com>
2015-03-18 10:37:56 -0700

    Make eslint fail *after* errors

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task('eslint', function () {
   gulp.src('./modules/**/*.js')
     .pipe(eslint())
     .pipe(eslint.formatEach('stylish', process.stderr))
-    .pipe(eslint.failOnError());
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('check', ['eslint']);
