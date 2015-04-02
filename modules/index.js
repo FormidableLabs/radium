425fea1bb1bdf17e6285b7e7ba0312cf77a59ec3
Ian Obermiller <iano@fb.com>
2015-04-02 00:54:22 -0700

    Forgot to remove media query exports

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,2 @@
-exports.MatchMediaBase = require('./mixins/match-media-base');
-exports.MatchMediaItem = require('./mixins/match-media-item');
 exports.Style = require('./components/style');
 exports.wrap = require('./wrap');
