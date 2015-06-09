70ef67a543c4f0f7653e14f158a7d86a827d1af2
Polgár András <azazdeaz@gmail.com>
2015-06-09 16:06:06 +0200

    add basic suport for madiaQuery aliases
    added functions:
    Config.setMediaQueryAlias(alias: string, query: string)
    Config.getMediaQueryByAlias(alias: string)
    Config.isMediaQueryAlias(alias: string)

--- modules/config.js
+++ modules/config.js
@@ -7,6 +7,7 @@ var ExecutionEnvironment = require('exenv');
 var _matchMediaFunction = ExecutionEnvironment.canUseDOM &&
   window &&
   window.matchMedia;
+var _mediaQueryAliases = new Map();
 
 module.exports = {
   canMatchMedia () {
@@ -19,5 +20,24 @@ module.exports = {
 
   setMatchMedia (nextMatchMediaFunction: Function) {
     _matchMediaFunction = nextMatchMediaFunction;
+  },
+
+  setMediaQueryAlias (alias: string, query: string) {
+    if (alias.indexOf('@') !== 0) {
+      throw new Error('alias should start with "@"');
+    }
+    if (query.indexOf('@media ') !== 0) {
+      throw new Error('query should start with "@media "');
+    }
+
+    _mediaQueryAliases.set(alias, query);
+  },
+
+  getMediaQueryByAlias (alias: string) {
+    return _mediaQueryAliases.get(alias);
+  },
+
+  isMediaQueryAlias (alias: string):boolean {
+    return _mediaQueryAliases.has(alias);
   }
 };
