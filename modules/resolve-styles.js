1ce3de2f4d04ceefcc2e4edf34416c3df6b0ba94
Ian Obermiller <ian@obermillers.com>
2015-07-13 23:29:25 +0100

    Fix setStyleState regression in b1fa309
    Oh React. If you call `setState` twice during the same execution frame,
    the second time, `this.state` will be the same as the first time. Thus,
    the second time will clobber the changes made the first time. This is a
    known issue: https://github.com/facebook/react/issues/122
    
    Btw, I noticed this when running the examples and trying the one with
    all the boxes; mouseLeave on one box gets called in the same frame as
    mouseEnter on another, and since all the boxes are in one component,
    they clobbered each others state, and everyone stayed hovered. Too lazy
    to write a proper test for this right now.

--- modules/resolve-styles.js
+++ modules/resolve-styles.js
@@ -28,9 +28,13 @@ var _getStyleState = function (component, key, value) {
 };
 
 var _setStyleState = function (component, key, newState) {
-  var existing = component.state && component.state._radiumStyleState || {};
+  var existing = component._lastRadiumState ||
+    component.state && component.state._radiumStyleState || {};
+
   var state = { _radiumStyleState: {...existing} };
   state._radiumStyleState[key] = {...state._radiumStyleState[key], ...newState};
+
+  component._lastRadiumState = state;
   component.setState(state);
 };
 
