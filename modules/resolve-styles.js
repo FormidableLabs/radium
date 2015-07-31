ddfc525a55fed8af8d0546c3737fe00687d8854f
Bob Renwick <bob@editd.com>
2015-07-31 11:54:40 +0100

    removing all edits to resolveStyles, they're too magic

--- modules/resolve-styles.js
+++ modules/resolve-styles.js
@@ -144,7 +144,6 @@ var _cloneElement = function (renderedElement, newProps, newChildren) {
 var resolveStyles = function (
   component: any, // ReactComponent, flow+eslint complaining
   renderedElement: any, // ReactElement
-  isEnhancedComponent: bool,
   existingKeyMap?: Object<string, string>
 ): any { // ReactElement
   existingKeyMap = existingKeyMap || {};
@@ -175,13 +174,13 @@ var resolveStyles = function (
       // If a React Element is an only child, don't wrap it in an array for
       // React.Children.map() for React.Children.only() compatibility.
       var onlyChild = React.Children.only(oldChildren);
-      newChildren = resolveStyles(component, onlyChild, false, existingKeyMap);
+      newChildren = resolveStyles(component, onlyChild, existingKeyMap);
     } else {
       newChildren = React.Children.map(
         oldChildren,
         function (child) {
           if (React.isValidElement(child)) {
-            return resolveStyles(component, child, false, existingKeyMap);
+            return resolveStyles(component, child, existingKeyMap);
           }
 
           return child;
@@ -205,20 +204,8 @@ var resolveStyles = function (
   }
 
   var props = renderedElement.props;
-  var className = props.className;
   var style = props.style;
   var newProps = {};
-
-  // If we have print styles we need to ensure that we activate them by giving
-  // them a class name
-  if (isEnhancedComponent && component.constructor.printStyles) {
-    if (className) {
-      className = className + ' ' + component.constructor.displayName;
-    } else {
-      className = component.constructor.displayName;
-    }
-    newProps.className = className;
-  }
 
   // Convenient syntax for multiple styles: `style={[style1, style2, etc]}`
   // Ignores non-objects, so you can do `this.state.isCool && styles.cool`.
@@ -357,7 +344,7 @@ var resolveStyles = function (
       newProps.style = Prefixer.getPrefixedStyle(style);
       return _cloneElement(renderedElement, newProps, newChildren);
     } else if (newChildren) {
-      return _cloneElement(renderedElement, newProps, newChildren);
+      return _cloneElement(renderedElement, {}, newChildren);
     }
 
     return renderedElement;
