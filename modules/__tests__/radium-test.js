605d585aa62261ac5e750fedce2db93855a7e230
Bob Renwick <bob.renwick@gmail.com>
2015-08-01 11:47:31 +0100

    adding ES6 component print styles test
    Unfortunately we can't use the decorator because we would need static
    class properties and we can't use them without using stage=0 on the babel
    settings

--- modules/__tests__/radium-test.js
+++ modules/__tests__/radium-test.js
@@ -2,8 +2,8 @@ var Radium = require('index.js');
 var React = require('react/addons');
 
 var {PrintStyle} = Radium;
+var {Component} = React;
 var TestUtils = React.addons.TestUtils;
-
 
 describe('Radium blackbox tests', () => {
   it('applies print styles through the PrintStyle component', () => {
@@ -24,6 +24,18 @@ describe('Radium blackbox tests', () => {
       }
     }));
 
+    class TestComponent2 extends Component {
+      render () {
+        return <div />;
+      }
+    }
+
+    TestComponent2.displayName = 'TestComponent2';
+    TestComponent2.printStyles = {
+      main: {color: 'black'}
+    };
+    Radium(TestComponent2);
+
     var output = TestUtils.renderIntoDocument(<PrintStyle />);
 
     var style = React.findDOMNode(
@@ -31,7 +43,7 @@ describe('Radium blackbox tests', () => {
     );
 
     expect(style.innerText).to.deep.equal(
-      '@media print{.Radium-TestComponent-foo{color: blue !important;}.Radium-TestComponent-bar{background: red !important;}}' // eslint-disable-line max-len
+      '@media print{.Radium-TestComponent-foo{color: blue !important;}.Radium-TestComponent-bar{background: red !important;}.Radium-TestComponent2-main{color: black !important;}}' // eslint-disable-line max-len
     );
   });
 
