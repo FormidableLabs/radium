4c208ca0239540dd360267abb43447d282fdf850
Bob Renwick <bob.renwick@gmail.com>
2015-08-02 22:59:37 +0100

    indent of 2 spaces, not 4

--- modules/print-styles.js
+++ modules/print-styles.js
@@ -4,67 +4,67 @@ var allPrintStyles = {};
 var listeners = [];
 
 var addListener = function (listener: Function): void {
-    var listenerIndex = listeners.indexOf(listener);
+  var listenerIndex = listeners.indexOf(listener);
 
-    if (listenerIndex === -1) {
-        listeners.push(listener);
-    }
+  if (listenerIndex === -1) {
+    listeners.push(listener);
+  }
 };
 
 var removeListener = function (listener: Function): void {
-    var listenerIndex = listeners.indexOf(listener);
+  var listenerIndex = listeners.indexOf(listener);
 
-    if (listenerIndex > -1) {
-        listeners.splice(listenerIndex, 1);
-    }
+  if (listenerIndex > -1) {
+    listeners.splice(listenerIndex, 1);
+  }
 };
 
 var emitChange = function () {
-    listeners.forEach(listener => listener());
+  listeners.forEach(listener => listener());
 };
 
 var importantValues = function (styleObj) {
-    var importantStyleObj = {};
+  var importantStyleObj = {};
 
-    Object.keys(styleObj).forEach(key => {
-        var value = styleObj[key];
+  Object.keys(styleObj).forEach(key => {
+    var value = styleObj[key];
 
-        // This breaks unitless values but they'll be deprecated soon anyway
-        // https://github.com/facebook/react/issues/1873
-        value = `${value} !important`;
-        importantStyleObj[key] = value;
-    });
+    // This breaks unitless values but they'll be deprecated soon anyway
+    // https://github.com/facebook/react/issues/1873
+    value = `${value} !important`;
+    importantStyleObj[key] = value;
+  });
 
-    return importantStyleObj;
+  return importantStyleObj;
 };
 
 var addPrintStyles = function (Component: constructor) {
-    if (!Component.printStyles) {
-        return;
-    }
+  if (!Component.printStyles) {
+    return;
+  }
 
-    var printStyleClass = {};
+  var printStyleClass = {};
 
-    Object.keys(Component.printStyles).forEach((key) => {
-        var styles = Component.printStyles[key];
-        var className = `Radium-${Component.displayName}-${key}`;
-        allPrintStyles[`.${className}`] = importantValues(styles);
-        printStyleClass[key] = className;
-    });
+  Object.keys(Component.printStyles).forEach((key) => {
+    var styles = Component.printStyles[key];
+    var className = `Radium-${Component.displayName}-${key}`;
+    allPrintStyles[`.${className}`] = importantValues(styles);
+    printStyleClass[key] = className;
+  });
 
-    // Allows for lazy loading of JS that then calls Radium to update the
-    // print styles
-    emitChange();
-    return printStyleClass;
+  // Allows for lazy loading of JS that then calls Radium to update the
+  // print styles
+  emitChange();
+  return printStyleClass;
 };
 
 var getPrintStyles = function (): Object  {
-    return allPrintStyles;
+  return allPrintStyles;
 };
 
 module.exports = {
-    addPrintStyles,
-    getPrintStyles,
-    addListener,
-    removeListener
+  addPrintStyles,
+  getPrintStyles,
+  addListener,
+  removeListener
 };
