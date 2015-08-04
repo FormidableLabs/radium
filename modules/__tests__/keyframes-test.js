e31e89a58124b4711328afadac1b017045a457b2
Andreas Savvides <asavvides@twitter.com>
2015-08-04 10:25:59 +0100

    Update tests & add test to showcase working without component

--- modules/__tests__/keyframes-test.js
+++ modules/__tests__/keyframes-test.js
@@ -49,7 +49,7 @@ describe('keyframes', () => {
     expect(document.createElement).not.to.have.been.called;
     expect(document.head.appendChild).not.to.have.been.called;
 
-    var name = keyframes(MyComponent, {});
+    var name = keyframes({}, MyComponent);
 
     expect(name.length).to.be.greaterThan(0);
   });
@@ -65,7 +65,7 @@ describe('keyframes', () => {
     expect(document.createElement).not.to.have.been.called;
     expect(document.head.appendChild).not.to.have.been.called;
 
-    var name = keyframes(MyComponent, {});
+    var name = keyframes({}, MyComponent);
 
     expect(name.length).to.be.greaterThan(0);
   });
@@ -75,7 +75,16 @@ describe('keyframes', () => {
       'exenv': exenv,
       './prefixer': require('__mocks__/prefixer.js')
     });
-    var name = keyframes(MyComponent, {});
+    var name = keyframes({}, MyComponent);
+    expect(name.length).to.be.greaterThan(0);
+  });
+
+  it('does not always require a component', () => {
+    var keyframes = require('inject?-./create-markup-for-styles!keyframes.js')({
+      'exenv': exenv,
+      './prefixer': require('__mocks__/prefixer.js')
+    });
+    var name = keyframes({});
     expect(name.length).to.be.greaterThan(0);
   });
 
@@ -84,7 +93,7 @@ describe('keyframes', () => {
       'exenv': exenv,
       './prefixer': require('__mocks__/prefixer.js')
     });
-    var name = keyframes(MyComponent, {});
+    var name = keyframes({}, MyComponent);
 
     expect(styleElement.sheet.insertRule.lastCall.args).to.deep.equal([
       '@-webkit-keyframes ' + name + ' {\n\n}\n',
@@ -98,7 +107,7 @@ describe('keyframes', () => {
       'exenv': exenv,
       './prefixer': require('__mocks__/prefixer.js')
     });
-    var name = keyframes(MyComponent, {});
+    var name = keyframes({}, MyComponent);
 
     expect(styleElement.sheet.insertRule.lastCall.args).to.deep.equal([
       '@keyframes ' + name + ' {\n\n}\n',
@@ -111,14 +120,14 @@ describe('keyframes', () => {
       'exenv': exenv,
       './prefixer': require('__mocks__/prefixer.js')
     });
-    var name = keyframes(MyComponent, {
+    var name = keyframes({
       from: {
         width: 100
       },
       to: {
         width: 200
       }
-    });
+    }, MyComponent);
 
     expect(styleElement.sheet.insertRule.lastCall.args).to.deep.equal([
 `@-webkit-keyframes ${name} {
