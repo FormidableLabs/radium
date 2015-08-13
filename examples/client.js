4ef58b61443c895fbc22f68b95d293568a3cfa08
jurgob <jurgo.boemo@gmail.com>
2015-08-13 01:24:05 +0100

    add isomorphic example

---
+++ examples/client.js
@@ -0,0 +1,4 @@
+var React = require('react');
+import App from './app.jsx'
+
+React.render(<App />, document.getElementById('app'));
---
+++ examples/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import React from "react";
+
+import App from './app.jsx'
+
+
+const app = express();
+
+app.set('views', './examples');
+app.set('view engine', 'jade');
+
+app.all('/*', function (req, res, next) {
+  console.log('access to : ', req.url)
+  next()
+});
+
+
+console.log(__dirname+'/bundle.js')
+app.use('/js/bundle.js', express.static(__dirname+'/bundle.js'));
+// app.use('/css', express.static(__dirname+'/../../public/css'));
+// app.use('/img', express.static(__dirname+'/../../public/img'));
+// app.use('/favicon.ico', express.static(__dirname+'/../../public/img/favicon.ico'));
+
+
+
+
+app.get('/*', function (req, res) {
+
+  console.log('generic access : ', req.url)
+
+  let content = React.renderToString((<App />));
+  res.render('index', { content: content });;
+
+});
+
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
