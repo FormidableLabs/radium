65a99faea6e2f29a74e8b58cfca39f2258fa3a6c
Harry Ledley <harry@actioniq.co>
2015-08-18 09:50:35 -0400

    getStateKey should return "main" when empty string is passed in

--- modules/get-state-key.js
+++ modules/get-state-key.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 var getStateKey = function (elementKey: ?string): string {
-  return elementKey === null || elementKey === undefined ?
+  return elementKey === null || elementKey === undefined || elementKey === '' ?
     'main' :
     elementKey.toString();
 };
