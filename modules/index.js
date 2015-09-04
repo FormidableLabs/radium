c796764e855c20902dace642b5158c05ff95082d
Ian Obermiller <ian@obermillers.com>
2015-09-05 00:06:55 +0100

    Export plugins

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,8 @@
 var Enhancer = require('./enhancer');
 
 module.exports = (ComposedComponent: constructor) => Enhancer(ComposedComponent);
-module.exports.Style = require('./components/style');
+module.exports.Plugins = require('./plugins');
 module.exports.PrintStyleSheet = require('./components/print-style-sheet');
+module.exports.Style = require('./components/style');
 module.exports.getState = require('./get-state');
 module.exports.keyframes = require('./keyframes');
