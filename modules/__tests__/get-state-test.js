3d02393d8ef4ffdea37d61bb7917a0a6249ccf88
Ian Obermiller <ian@obermillers.com>
2015-10-17 06:07:44 -0700

    Fix getState test

--- modules/__tests__/get-state-test.js
+++ modules/__tests__/get-state-test.js
@@ -1,12 +1,6 @@
 var getState = require('get-state.js');
 
 describe('getState', function () {
-  it('throws on unknown value', function () {
-    expect(function () {
-      getState({}, null, 'unknown');
-    }).to.throw();
-  });
-
   it('successfully gets the state if passed number zero', function () {
     var result = getState(
       {_radiumStyleState: {'0': {':hover': true}}},
