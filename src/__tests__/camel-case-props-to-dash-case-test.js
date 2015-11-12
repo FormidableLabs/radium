a14953f89f02ced58819be6ba7ab672da04a424d
PallasKatze <pallas.katze@gmail.com>
2015-11-12 17:26:44 +0200

    Fix -ms vendor prefix
    Closes FormidableLabs/radium#413

--- src/__tests__/camel-case-props-to-dash-case-test.js
+++ src/__tests__/camel-case-props-to-dash-case-test.js
@@ -4,12 +4,14 @@ describe('camelCasePropsToDashCase', function() {
   it('converts to dash case correctly', function() {
     const result = camelCasePropsToDashCase({
       borderLeft: '1px solid black',
-      WebkitBoxSizing: 'border-box'
+      WebkitBoxSizing: 'border-box',
+      msTransform: 'rotate(90deg)'
     });
 
     expect(result).to.deep.equal({
       'border-left': '1px solid black',
-      '-webkit-box-sizing': 'border-box'
+      '-webkit-box-sizing': 'border-box',
+      '-ms-transform': 'rotate(90deg)'
     });
   });
 });
--- src/camel-case-props-to-dash-case.js
+++ src/camel-case-props-to-dash-case.js
@@ -1,9 +1,11 @@
 /* @flow */
 
 const _camelCaseRegex = /([a-z])?([A-Z])/g;
+
 const _camelCaseReplacer = function(match, p1, p2) {
   return (p1 || '') + '-' + p2.toLowerCase();
 };
+
 const _camelCaseToDashCase = function(s) {
   return s.replace(_camelCaseRegex, _camelCaseReplacer);
 };
@@ -12,7 +14,14 @@ const camelCasePropsToDashCase = function(prefixedStyle: Object): Object {
   // Since prefix is expected to work on inline style objects, we must
   // translate the keys to dash case for rendering to CSS.
   return Object.keys(prefixedStyle).reduce((result, key) => {
-    result[_camelCaseToDashCase(key)] = prefixedStyle[key];
+    let dashCaseKey = _camelCaseToDashCase(key);
+
+    // Fix IE vendor prefix
+    if (/^ms-/.test(dashCaseKey)) {
+      dashCaseKey = `-${dashCaseKey}`;
+    }
+
+    result[dashCaseKey] = prefixedStyle[key];
     return result;
   }, {});
 };
