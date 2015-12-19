dc34849d17217d813aa7d8c5686f52d2fd324ee0
Ian Obermiller <ian@obermillers.com>
2015-12-19 14:28:07 -0800

    StyleSheet to ES6

--- src/components/style-sheet.js
+++ src/components/style-sheet.js
@@ -1,45 +1,48 @@
 /* @flow */
 
-import React from 'react';
+import React, {Component} from 'react';
 
 import Style from './style.js';
 import StyleKeeper from '../style-keeper';
 
-const StyleSheet = React.createClass({
-  _subscription: ((null: any): {remove: () => void}),
+export default class StyleSheet extends Component {
+  _subscription: {remove: () => void};
 
-  contextTypes: {
+  // $FlowStaticPropertyWarning
+  static contextTypes = {
     _radiumStyleKeeper: React.PropTypes.instanceOf(StyleKeeper)
-  },
+  };
 
-  getInitialState() {
-    return this._getCSSState();
-  },
+  constructor() {
+    super();
+
+    this.state = this._getCSSState();
+  }
 
   componentDidMount() {
     this._subscription = this.context._radiumStyleKeeper.subscribe(
       this._onChange
     );
     this._onChange();
-  },
+  }
 
   componentWillUnmount() {
-    this._subscription.remove();
-  },
+    if (this._subscription) {
+      this._subscription.remove();
+    }
+  }
 
   _getCSSState(): {css: string} {
     return {css: this.context._radiumStyleKeeper.getCSS()};
-  },
+  }
 
   _onChange() {
     this.setState(this._getCSSState());
-  },
+  }
 
   render(): ReactElement {
     return (
       <style dangerouslySetInnerHTML={{__html: this.state.css}} />
     );
   }
-});
-
-export default StyleSheet;
+}
