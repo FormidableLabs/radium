7993ab39e47ffb708062b186b1c8854621d81217
Ian Obermiller <ian@obermillers.com>
2015-12-24 14:19:45 -0800

    Move hash into its own module

--- src/keyframes.js
+++ src/keyframes.js
@@ -1,25 +1,12 @@
 /* @flow */
 
 import cssRuleSetToString from './css-rule-set-to-string';
+import hash from './hash';
 
 export type Keyframes = {
   __radiumKeyframes: bool,
   __process(userAgent?: string): {animationName: string, css: string},
 };
-
-// a simple djb2 hash based on hash-string:
-// https://github.com/MatthewBarker/hash-string/blob/master/source/hash-string.js
-function hash(text) {
-  let hashValue = 5381;
-  let index = text.length - 1;
-
-  while (index) {
-    hashValue = (hashValue * 33) ^ text.charCodeAt(index);
-    index -= 1;
-  }
-
-  return (hashValue >>> 0).toString(16);
-}
 
 export default function keyframes(
   keyframeRules: {[percentage: string]: {[key: string]: string|number}},
