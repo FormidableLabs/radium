d6fa0b54ab1fdec238fa11e110e4a2bad4544b5a
Ian Obermiller <ian@obermillers.com>
2016-01-02 07:03:10 -0800

    Add space before !important

--- src/append-important-to-each-value.js
+++ src/append-important-to-each-value.js
@@ -2,7 +2,7 @@ export default function appendImportantToEachValue(style) {
   return Object.keys(style).reduce((result, key) => {
     // This breaks unitless values but they'll be deprecated soon anyway
     // https://github.com/facebook/react/issues/1873
-    result[key] = style[key] + '!important';
+    result[key] = style[key] + ' !important';
     return result;
   }, {});
 }
