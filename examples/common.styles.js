db649511f998aee6bfa34d4392a038443bdd2491
William Boman <william@redwill.se>
2016-01-29 18:38:41 +0100

    examples: add spread operator tip

---
+++ examples/common.styles.js
@@ -0,0 +1,15 @@
+module.exports = {
+  resetListStyle: {
+    listStyle: 'none'
+  },
+  resetBoxModel: {
+    marginTop: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    marginLeft: 0,
+    paddingTop: 0,
+    paddingRight: 0,
+    paddingBottom: 0,
+    paddingLeft: 0
+  }
+};
