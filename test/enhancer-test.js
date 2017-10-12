72bb03a48930c740816b37d57b6a381433f10333
Ryan Roemer <ryan@loose-bits.com>
2017-10-11 22:07:22 -0700

    WIP: Have a repro of the error for real regression test.

--- test/enhancer-test.js
+++ test/enhancer-test.js
@@ -15,7 +15,11 @@ describe('Enhancer', () => {
   // Regression test - `Radium wrapping not compatible with native classes?`
   // https://github.com/FormidableLabs/radium/issues/576
   it('handles native ES classes', () => {
-    class Composed extends React.Component {}
+    class Composed extends React.Component {
+      render() {
+        return React.createElement('div');
+      }
+    }
     const Enhanced = Enhancer(Composed);
     const instance = new Enhanced();
 
