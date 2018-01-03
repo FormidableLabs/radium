066e3b08c24965c88f89903d6764f36bc6c63d7b
Stefan Huynh <huynh.stefan@gmail.com>
2018-01-03 13:40:49 -0800

    Abstract getRadiumStyleState

---
+++ src/get-radium-style-state.js
@@ -0,0 +1,8 @@
+/* @flow */
+
+const getRadiumStyleState = function(component) {
+  return component._lastRadiumState ||
+    (component.state && component.state._radiumStyleState) || {};
+};
+
+export default getRadiumStyleState;
