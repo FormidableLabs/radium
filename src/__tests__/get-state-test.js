809a8faec570aec15fdb0fcb400b2f10959ef162
Stefan Huynh <huynh.stefan@gmail.com>
2018-01-05 17:55:30 -0800

    Add tests for cleanStateKey

--- src/__tests__/get-state-test.js
+++ src/__tests__/get-state-test.js
@@ -1,7 +1,7 @@
 import getState from 'get-state.js';
 
-describe('getState', function() {
-  it('successfully gets the state if passed number zero', function() {
+describe('getState', () => {
+  it('successfully gets the state if passed number zero', () => {
     const result = getState(
       {_radiumStyleState: {'0': {':hover': true}}},
       0,
