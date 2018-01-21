a159d40db7a1b06b8e3463d74afb5f10cf270f1a
Ryan Roemer <ryan@loose-bits.com>
2018-01-21 12:50:32 -0800

    WIP: Add failing regression tests.

--- test/radium-test.js
+++ test/radium-test.js
@@ -1,25 +1,11 @@
 'use strict';
 
 const React = require('react');
-const renderToString = require('react-dom/server').renderToString;
 
 const Radium = require('../lib/index');
+const render = require('./utils').render;
 
-// TODO: ABSTRADT?
-const render = (Component, props) =>
-  renderToString(
-    React.createElement(
-      Component,
-      Object.assign(
-        {
-          radiumConfig: {userAgent: 'testy-mctestface'}
-        },
-        props
-      )
-    )
-  );
-
-describe.only('Radium blackbox SSR tests', () => {
+describe('Radium blackbox SSR tests', () => {
   it('sets up initial state', () => {
     const Enhanced = Radium(React.Component);
     const instance = new Enhanced();
@@ -27,23 +13,41 @@ describe.only('Radium blackbox SSR tests', () => {
     expect(instance.state).to.deep.equal({_radiumStyleState: {}});
   });
 
-  it('correctly inline prefixes', () => {
-    class Composed extends React.Component {
-      render() {
-        return React.createElement('div', {
-          style: {display: 'flex'}
-        });
+  describe('inline prefixes', () => {
+    let Wrapped;
+
+    beforeEach(() => {
+      class Composed extends React.Component {
+        render() {
+          return React.createElement('div', {
+            style: {display: 'flex'}
+          });
+        }
       }
-    }
 
-    const Wrapped = Radium(Composed);
-    const rendered = render(Wrapped);
-    console.log("TODO", rendered);
-    // expect(instance).to.have.deep.property('props.style').that.eql({
-    //   flex: "TODO"
-    // });
-    // TODO HERE -- what assert?
-    // TODO: Add display styles flex
+      Wrapped = Radium(Composed);
+    });
+
+    it('handles no user agent', () => {
+      const rendered = render(Wrapped);
+      expect(rendered).to.contain(
+        'style="display:-webkit-box;-moz-box;-ms-flexbox;-webkit-flex;flex"');
+    });
+
+    it('handles non-matching user agent', () => {
+      const rendered = render(Wrapped, {
+        userAgent: 'testy-mctestface'
+      });
+      expect(rendered).to.contain(
+        'style="display:-webkit-box;-moz-box;-ms-flexbox;-webkit-flex;flex"');
+    });
+
+    it('handles matching user agent', () => {
+      const rendered = render(Wrapped, {
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, ' +
+          'like Gecko) Chrome/63.0.3239.84 Safari/537.36'
+      });
+      expect(rendered).to.contain('style="display:flex"');
+    });
   });
-
 });
