d59315f6b82fc0c3b1e17ed0eb2dddf41acf4db7
Ryan Roemer <ryan@loose-bits.com>
2018-01-24 12:46:40 -0800

    Add back polyfills for ie9 on Appveyor

--- karma.conf.js
+++ karma.conf.js
@@ -4,8 +4,17 @@ module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['mocha', 'sinon-chai'],
-    files: ['src/__tests__/**/*.js'],
+    files: [
+      // Polyfills for IE9 in React 16.
+      require.resolve('core-js/es6/map'),
+      require.resolve('core-js/es6/set'),
+      'src/__tests__/**/*.js'
+    ],
     preprocessors: {
+      [path.join(
+        path.dirname(require.resolve('core-js/package.json')),
+        'es6/**/*.js' // eslint-disable-line prettier/prettier
+      )]: ['webpack'],
       'src/__tests__/**/*.js': ['webpack']
     },
     webpack: {
