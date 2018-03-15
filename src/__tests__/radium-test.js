e5b17df45c78dcf0b7df93af6569c66228a56cd2
Ryan Roemer <ryan@loose-bits.com>
2018-03-15 13:19:23 -0700

    Support ES7 arrow functions on React classes. (#977)
    - Fixes #738
    - This is hacky, but possibly **acceptably** hacky.
    - Detect and transfer instance method properties from arrow functions to a copy of the prototype (where by definition they don't exist) to allow property inheritance call chaining by `RadiumEnhancer` which otherwise doesn't get its methods called.

--- src/__tests__/radium-test.js
+++ src/__tests__/radium-test.js
@@ -730,6 +730,66 @@ describe('Radium blackbox tests', () => {
     expect(div.style.color).to.equal('red');
   });
 
+  // Regression test: https://github.com/FormidableLabs/radium/issues/738
+  it('works with arrow-based render methods in components', () => {
+    class TestComponent extends Component {
+      render = () => {
+        return (
+          <div style={{color: 'blue', ':hover': {color: 'red'}}}>
+            {this.props.children}
+          </div>
+        );
+      };
+    }
+
+    const Wrapped = Radium(TestComponent);
+    const output = TestUtils.renderIntoDocument(<Wrapped>hello world</Wrapped>);
+
+    // Check prototype is not mutated.
+    expect(TestComponent.prototype).to.not.have.property('render');
+
+    const div = getElement(output, 'div');
+
+    expect(div.style.color).to.equal('blue');
+    expect(div.innerText).to.equal('hello world');
+
+    TestUtils.SimulateNative.mouseOver(div);
+
+    expect(div.style.color).to.equal('red');
+  });
+
+  // Regression test: https://github.com/FormidableLabs/radium/issues/738
+  it('works with arrow-based render methods in components with complex inheritence', () => {
+    class First extends Component {}
+    class Second extends First {}
+    class TestComponent extends Second {
+      render = () => {
+        return (
+          <div style={{color: 'blue', ':hover': {color: 'red'}}}>
+            {this.props.children}
+          </div>
+        );
+      };
+    }
+
+    const Wrapped = Radium(TestComponent);
+    const output = TestUtils.renderIntoDocument(<Wrapped>hello world</Wrapped>);
+
+    // Check prototypes are not mutated.
+    expect(First.prototype).to.not.have.property('render');
+    expect(Second.prototype).to.not.have.property('render');
+    expect(TestComponent.prototype).to.not.have.property('render');
+
+    const div = getElement(output, 'div');
+
+    expect(div.style.color).to.equal('blue');
+    expect(div.innerText).to.equal('hello world');
+
+    TestUtils.SimulateNative.mouseOver(div);
+
+    expect(div.style.color).to.equal('red');
+  });
+
   it('works fine if passing null, undefined, or false in style', () => {
     const TestComponent = Radium(() => (
       <div style={{background: undefined, border: false, color: null}} />
