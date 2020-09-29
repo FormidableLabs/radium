3b696d57f52fdf757ab8725c0d85d62def94ce24
Kyle Cesmat <kyle.cesmat@formidable.com>
2020-09-29 11:06:25 -0700

    Drop ie9 support in test

--- karma.conf.ie.js
+++ karma.conf.ie.js
@@ -2,12 +2,6 @@ module.exports = function(config) {
   require('./karma.conf.js')(config);
   config.set({
     plugins: config.plugins.concat('karma-ie-launcher'),
-    browsers: ['IE9'],
-    customLaunchers: {
-      IE9: {
-        base: 'IE',
-        'x-ua-compatible': 'IE=EmulateIE9'
-      }
-    }
+    browsers: ['IE']
   });
 };
